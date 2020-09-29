71e0e85d8c8f7d8e32f5bd6b9736e2ed4fa037f3
unknown <marcos.devel@gmail.com>
2020-09-28 22:32:39 -0300

    update...

--- src/pages/ZeroWhisper/index.js
+++ src/pages/ZeroWhisper/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 
-// import Header from './Header';
-// import Body from './Body';
-
-// import * as S from './style';
 import { Redirect } from 'react-router-dom';
 
 const ZeroWhisper = props => {
   return <Redirect to={'/example'} />;
-  // return (
-  //   <S.Container>
-  //     <Header />
-  //     <Body />
-  //   </S.Container>
-  // );
 };
 
 export default ZeroWhisper;
