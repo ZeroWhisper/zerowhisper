4b526e2d3dec1c7b7b35f4dfb9c27119aead0483
unknown <marcos.devel@gmail.com>
2020-11-11 19:49:38 -0300

    update-override

--- config-overrides.js
+++ config-overrides.js
@@ -3,17 +3,17 @@
 const {
   override,
   addBabelPlugin,
-  addWebpackModuleRule
-} = require("customize-cra");
+  addWebpackModuleRule,
+} = require('customize-cra');
 
 const moduleResolver = [
-  "babel-plugin-module-resolver",
+  'babel-plugin-module-resolver',
   {
-    root: ["./src/"],
+    root: ['./src/'],
     alias: {
-      "~": "./src"
-    }
-  }
+      '~': './src',
+    },
+  },
 ];
 
 const fontLoad = {
@@ -22,18 +22,78 @@ const fontLoad = {
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
       use: [
         {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[name].[ext]",
-            outputPath: "fonts/"
-          }
-        }
-      ]
-    }
-  ]
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = override(
-  addBabelPlugin(moduleResolver)
+  addBabelPlugin(moduleResolver),
   // addWebpackModuleRule(fontLoad)
 );
+
+// const path = require('path');
+
+// const {
+//   override,
+//   addBabelPlugin,
+//   // addWebpackModuleRule,
+//   addWebpackAlias,
+// } = require('customize-cra');
+
+// const plugins = [
+//   'babel-plugin-module-resolver',
+//   {
+//     root: path.resolve(__dirname, 'src'),
+//     alias: {
+//       test: './test',
+//       '~': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// ];
+
+// const moduleResolver = [
+//   'babel-plugin-module-resolver',
+//   {
+//     root: ['./src/'],
+//     alias: {
+//       '~': './src',
+//     },
+//   },
+// ];
+
+// const aliasName = {
+//   ['~']: path.resolve(__dirname, 'src'),
+//   ['@ui']: path.resolve(__dirname, 'src', 'ui'),
+//   ['@src']: path.resolve(__dirname, 'src'),
+// };
+
+// const fontLoad = {
+//   rules: [
+//     {
+//       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {
+//             name: '[name].[ext]',
+//             outputPath: 'fonts/',
+//           },
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// module.exports = override(
+//   addBabelPlugin(moduleResolver),
+//   // addBabelPlugin(plugins),
+//   // addWebpackModuleRule(fontLoad)
+//   // addWebpackAlias(aliasName),
+// );
