0d45f2faa5065ac3f0f0e36066eb5cce34fbc17d
ZeroWhisper <marcos.devel@gmail.com>
2021-02-17 14:52:47 -0300

    update

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import * as Redux from 'react-redux';
+
+// import * as S from './style';
+
+interface IApp extends React.HTMLAttributes<HTMLDivElement> {}
+
+function App(_props: IApp) {
+  // const {} = _props;
+
+  return <React.Fragment>I AM App</React.Fragment>;
+}
+
+export default App;
