2602bf52eebb8bf3a3beb8b4daaa7aed07a84334
Marcos <marcos.devel@gmail.com>
2021-04-24 16:11:55 -0300

    news

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
-import { store } from './store';
-import GlobalStyle from './styles/global';
-import Routes from './routes';
-import reactotron from '~/config/reactotron';
+import AppContainer from '~/components/AppContainer';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
 
-reactotron.getInstanceReactotron();
+// import { store } from './store';
+// import GlobalStyle from './styles/global';
+// import Routes from './routes';
+// import reactotron from '~/config/reactotron';
+
+// reactotron.getInstanceReactotron();
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </Provider>
-  );
+  // return <AppContainer />
+  // return (
+  //   <Provider store={store}>
+  //     <GlobalStyle />
+  //     <BrowserRouter>
+  //       <Routes />
+  //     </BrowserRouter>
+  //   </Provider>
+  // );
 };
 
 export default App;
