0d411555e8446f4a9430e31d70da9244b49b2163
Marcos <marcos.devel@gmail.com>
2021-05-24 17:39:30 -0300

    update

---
+++ src/app_redux/index.d.ts
@@ -0,0 +1,18 @@
+import 'styled-componets';
+import 'react-redux';
+
+import { RootState } from './modules/rootReducer';
+
+declare module 'react-redux' {
+  export interface DefaultRootState extends RootState {}
+}
+
+// import { CustomThemeType } from '../components/Organisms/AppContainer/theme.type';
+
+// type ThemeLight = CustomThemeType;
+
+declare module 'styled-components' {
+  export interface DefaultTheme {}
+}
+
+export {};
