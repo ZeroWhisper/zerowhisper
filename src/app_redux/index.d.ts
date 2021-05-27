a8f3ee2b390777ce6af39a1fadbbb08a06134b05
Marcos <marcos.devel@gmail.com>
2021-05-27 13:50:07 -0300

    update

--- src/app_redux/index.d.ts
+++ src/app_redux/index.d.ts
@@ -2,6 +2,7 @@ import 'react-redux';
 
 import { RootState } from './modules/rootReducer';
 
+// talvez não funcione =(
 declare module 'react-redux' {
   export interface DefaultRootState extends RootState {}
 }
