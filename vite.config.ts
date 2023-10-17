048865c09812570b51109fd706eca1370ed0fef7
ZeroWhisper <marcos.devel@gmail.com>
2023-10-16 23:26:47 -0300

    feat: update relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// Absolute Paths: https://dev.to/andrewezeani/how-to-create-absolute-imports-in-vite-react-app-a-step-by-step-guide-28co
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "~": "/src",
+      src: "/src",
+    },
+  },
+});
