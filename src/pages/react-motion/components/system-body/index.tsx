fd57600c3b215e04525978245434c6c2c4c6f12a
Marcos <marcos.cordeiro@brickabode.com>
2023-10-28 15:52:34 -0300

    feat: add a list feature

--- src/pages/react-motion/components/system-body/index.tsx
+++ src/pages/react-motion/components/system-body/index.tsx
@@ -1,5 +1,36 @@
 export const BodySystem = () => {
   // const {} = _props;
 
-  return <div style={{ display: "flex", flex: 1 }}>body</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        gap: "0.5rem",
+        margin: "1.1rem 0.5rem",
+        flexDirection: "column",
+      }}
+    >
+      <BadgeBorder>1</BadgeBorder>
+      <BadgeBorder>1</BadgeBorder>
+      <BadgeBorder>1</BadgeBorder>
+    </div>
+  );
+};
+
+export const BadgeBorder = ({ children }: any) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        borderRadius: "1rem",
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderColor: "white",
+        padding: "0.5rem",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
