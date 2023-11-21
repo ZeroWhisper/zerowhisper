241e03e7c30ad62b7b7c2d8fea3e401bc1c9316a
ZeroWhisper <marcos.devel@gmail.com>
2023-11-20 22:25:34 -0300

    chore: remove useless functions

---
+++ src/components/modals/showData.tsx
@@ -0,0 +1,33 @@
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+export const ShowData = () => {
+
+  const modalIsOpen = false;
+  const afterOpenModal = () => {};
+  const closeModal = () => {};
+
+  return (
+    <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        Teste
+      </Modal>
+    </div>
+  );
+};
